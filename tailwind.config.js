4d5e34c393990a2348c58da58600477d7eff3774
Prashantzzz <prashant2003prashant@gmail.com>
2024-02-11 10:19:01 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-  daisyui:{
-    themes: ["light", "dark", "valentine","coffee","forest","retro"],
+  plugins: [require("daisyui"), require("tailwind-scrollbar-daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "valentine", "coffee", "forest", "retro"],
   },
-}
+  variants: {
+    scrollbar: ["dark"],
+  },
+};
